75332d678c632e54a55979ca264e497892c8f429
Zander <swarnavabhattacharya24@gmail.com>
2023-12-01 16:54:14 +0530

    Added urls for anime info and each episode page

--- gogoanimeClone/gogoAnimeClone/website/urls.py
+++ gogoanimeClone/gogoAnimeClone/website/urls.py
@@ -5,4 +5,6 @@ from django.views.generic import TemplateView
 app_name = "website"
 urlpatterns = [
     path('', views.MainView.as_view()),
+    path('category/<str:anime_name>', views.AnimeView.as_view(), name='anime_detailed_view'),
+    path('<str:anime_name>', views.WatchView.as_view(), name='episode_view')
 ]
--- gogoanimeClone/gogoAnimeClone/website/views.py
+++ gogoanimeClone/gogoAnimeClone/website/views.py
@@ -9,4 +9,16 @@ class MainView(View):
     def get(self, request):
         return render(request, 'website/main.html')
 
+
+class AnimeView(View):
+
+    def get(self, request, anime_name):
+        return render(request, 'website/anime_detail.html')
+
+
+class WatchView(View):
+
+    def get(self, request, anime_name):
+        return render(request, 'website/play_anime.html')
+
 # Create your views here.
