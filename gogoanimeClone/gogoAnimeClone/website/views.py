608affbe07b8523fb053d02c532920317335f8d4
Zander <swarnavabhattacharya24@gmail.com>
2024-02-19 03:29:01 +0530

    Completed Landing Page with test data

--- gogoanimeClone/gogoAnimeClone/website/views.py
+++ gogoanimeClone/gogoAnimeClone/website/views.py
@@ -10,10 +10,27 @@ class MainView(View):
         main_anime = {'name': 'Solo Leveling', 'episode': 6}
         popular_anime = {'name': 'Solo Leveling', 'genres': ['Action', 'Shounen', 'Adult'], 'latest_episode': 6}
         anime_name = 'Solo Leveling'
+        top_anime = {'name': 'Solo Leveling', 'episode': 6}
+        ongoing = 'Solo Leveling'
+        genre = 'Adventure'
         page_data = {'summary': 'This is a summary',
-                     'mainAnime': [main_anime for x in range(1, 21)],
-                     'popularAnime': [popular_anime for i in range(1, 11)],
-                     'recentlyAdded': [anime_name for i in range(1, 41)]}
+                     'mainAnime': [main_anime for i in range(20)],
+                     'popularAnime': [popular_anime for i in range(10)],
+                     'recentlyAdded': [anime_name for i in range(40)],
+                     'topAnime': [top_anime for i in range(10)],
+                     'ongoing': [ongoing for i in range(100)],
+                     'genres': [genre for i in range(83)],
+                     'seasons': {2024: ['Winter'],
+                                 2023: ['Winter', 'Spring', 'Summer', 'Fall'],
+                                 2022: ['Winter', 'Spring', 'Summer', 'Fall'],
+                                 2021: ['Winter', 'Spring', 'Summer', 'Fall'],
+                                 2020: ['Winter', 'Spring', 'Summer', 'Fall'],
+                                 2019: ['Winter', 'Spring', 'Summer', 'Fall'],
+                                 2018: ['Winter', 'Spring', 'Summer', 'Fall'],
+                                 2017: ['Winter', 'Spring', 'Summer', 'Fall'],
+                                 2016: ['Winter', 'Spring', 'Summer', 'Fall'],
+                                 2015: ['Winter', 'Spring', 'Summer', 'Fall'],
+                                 2014: ['Winter', 'Spring', 'Summer', 'Fall']}}
         return render(request, 'website/main.html', page_data)
 
 
