8a90618c1020fdcba934591d993e9b6d2dab2982
Zander <swarnavabhattacharya24@gmail.com>
2024-03-01 15:57:48 +0530

    Fixed Data Models

--- gogoanimeClone/gogoAnimeClone/website/admin.py
+++ gogoanimeClone/gogoAnimeClone/website/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Genre, Lang, Type, Status, Year, Season, Episodes, Anime
+
+admin.site.register(Genre)
+admin.site.register(Lang)
+admin.site.register(Type)
+admin.site.register(Status)
+admin.site.register(Year)
+admin.site.register(Season)
+admin.site.register(Episodes)
+admin.site.register(Anime)
--- gogoanimeClone/gogoAnimeClone/website/models.py
+++ gogoanimeClone/gogoAnimeClone/website/models.py
@@ -28,14 +28,15 @@ class Season(models.Model):
 class Episodes(models.Model):
     name = models.CharField(max_length=30)
     episode_link = models.CharField(max_length=200)
+    anime = models.ForeignKey('Anime', on_delete=models.CASCADE, null=False, default=-1)
 
 
 class Anime(models.Model):
     anime_banner_link = models.CharField(max_length=200)
     type = models.ForeignKey('Type', on_delete=models.CASCADE, null=False)
     name = models.CharField(max_length=200)
-    summary = models.CharField(max_length=2000)
+    summary = models.TextField()
     genre = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True)
-    released = models.ForeignKey('Year', on_delete=models.CASCADE, null=False)
+    released = models.ForeignKey('Year', on_delete=models.SET_NULL, null=True)
     status = models.ForeignKey('Status', on_delete=models.SET_NULL, null=True)
-    other_names = models.CharField(max_length=200)
+    other_names = models.TextField()
